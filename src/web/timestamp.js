82e50d4aba5c676912fa464656acd3865e506d4d
Andrea Cardaci <cyrus.and@gmail.com>
2020-11-10 01:26:22 +0100

    Create global constant from local variable for clarity

--- src/web/timestamp.js
+++ src/web/timestamp.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 import './timestamp.scss';
 
+const STEPS = [
+    {threshold: 1000, label: 'ms'},
+    {threshold: 60, label: 's'},
+    {threshold: 60, label: 'm'},
+    {threshold: 24, label: 'h'},
+    {threshold: Infinity, label: 'd'}
+];
+
 export default class Timestamp extends React.Component {
     render() {
         return (
@@ -12,27 +20,19 @@ export default class Timestamp extends React.Component {
     }
 
     _formatTimestamp() {
-        const steps = [
-            {threshold: 1000, label: 'ms'},
-            {threshold: 60, label: 's'},
-            {threshold: 60, label: 'm'},
-            {threshold: 24, label: 'h'},
-            {threshold: Infinity, label: 'd'}
-        ];
-
         // if a maximum is provided use it to determine where to stop
         let stopAt = Infinity;
         let {maximum} = this.props;
         if (maximum) {
-            for (stopAt = 0; stopAt < steps.length && maximum; stopAt++) {
-                maximum = Math.floor(maximum / steps[stopAt].threshold);
+            for (stopAt = 0; stopAt < STEPS.length && maximum; stopAt++) {
+                maximum = Math.floor(maximum / STEPS[stopAt].threshold);
             }
         }
 
         // break the milliseconds timestamp in its components
         let milliseconds = this.props.frame.cumulativeDelay;
         const components = [];
-        for (const {threshold} of steps) {
+        for (const {threshold} of STEPS) {
             components.push(milliseconds % threshold);
             milliseconds = Math.floor(milliseconds / threshold);
         }
@@ -49,12 +49,12 @@ export default class Timestamp extends React.Component {
             let output = component.toString();
 
             // pad all the components except for the last
-            if (steps[index].threshold !== Infinity) {
-                output = output.padStart((steps[index].threshold - 1).toString().length, '0');
+            if (STEPS[index].threshold !== Infinity) {
+                output = output.padStart((STEPS[index].threshold - 1).toString().length, '0');
             }
 
             // apply the label
-            return output + steps[index].label;
+            return output + STEPS[index].label;
         }).slice(0, stopAt).reverse().join(':');
     }
 }
