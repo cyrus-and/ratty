f8214608559b19bd7b0a7233dc7c67cb04ecf6dd
Andrea Cardaci <cyrus.and@gmail.com>
2021-05-31 22:28:20 +0200

    Treat invalid regexps literally

--- src/web/regexps.js
+++ src/web/regexps.js
@@ -1,7 +1,17 @@
+function unicodeEscape(string) {
+    // turn each code point into its unicode representation
+    return Array.from(string).map((chunk) => {
+        return `\\u{${chunk.codePointAt(0).toString(16)}}`;
+    }).join('');
+}
+
 export function build(searchQuery, caseSensitivity, global) {
+    const baseFlags = `${caseSensitivity ? '' : 'i'}${global ? 'g' : ''}`;
+
     try {
-        return new RegExp(searchQuery, `${caseSensitivity ? '' : 'i'}${global ? 'g' : ''}`);
+        return new RegExp(searchQuery, baseFlags);
     } catch {
-        return null;
+        // treat invalid regexps literally
+        return new RegExp(unicodeEscape(searchQuery), `u${baseFlags}`);
     }
 }
