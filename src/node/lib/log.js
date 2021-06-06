07cc6857e98d2ea80770fd88fee670dce0e8028e
Andrea Cardaci <cyrus.and@gmail.com>
2021-06-06 14:54:36 +0200

    Change log messages style

--- src/node/lib/log.js
+++ src/node/lib/log.js
@@ -1,15 +1,15 @@
 const chalk = require('chalk');
 
 function log(marker, message) {
-    console.log(`[${marker}] ${message}`);
+    console.log(`${chalk.inverse(marker)} ${message}`);
 }
 
 function info(message) {
-    log(chalk.green('+'), message);
+    log(chalk.green(' INFO '), message);
 }
 
 function error(message) {
-    log(chalk.red('!'), message);
+    log(chalk.red(' ERROR '), message);
 }
 
 module.exports = {info, error};
